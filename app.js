a93131fa08d8c8aaac2ef34f6412aefed9f28a4a
dryisf <daryl.issoufi@gmail.com>
2020-05-17 22:27:17 +0200

    ajout compétences + à propos

---
+++ app.js
@@ -0,0 +1,23 @@
+let webSkills = document.querySelectorAll('.web-skill-link');
+let otherSkills = document.querySelectorAll('.other-skill-link');
+
+let webSkillImg = document.querySelector('.web-skill-img');
+let otherSkillImg = document.querySelector('.other-skill-img');
+
+let displayWebSkillImg = function (e) {
+  let skillName = e.target.innerHTML;
+  webSkillImg.src = `./assets/img/${skillName}.png`;
+}
+
+let displayOtherSkillImg = function (e) {
+  let skillName = e.target.innerHTML;
+  otherSkillImg.src = `./assets/img/${skillName}.png`;
+}
+
+for (const skill of webSkills) {
+  skill.addEventListener('mouseover', displayWebSkillImg);
+}
+
+for (const skill of otherSkills) {
+  skill.addEventListener('mouseover', displayOtherSkillImg);
+}
